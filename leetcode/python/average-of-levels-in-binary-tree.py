a7e96cf32381806f3075fbf839a876ff0cf97efd
Luc <lucwsomers@gmail.com>
2019-07-02 08:09:10 -0300

    binary search problem

---
+++ leetcode/python/average-of-levels-in-binary-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if root is not None:
+            output = []
+            depth = 0
+
+            def trav(node, depth):
+                depth += 1
+                if len(output) < depth:
+                    output.append([])
+                output[depth - 1].append(node.val)
+                if node.left is not None:
+                    trav(node.left, depth)
+                if node.right is not None:
+                    trav(node.right, depth)
+            trav(root, depth)
+            return [sum(x)/len(x) for x in output]
+        else:
+            return []
