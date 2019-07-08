7566bfd879bb1ef729923955f0265d8bb5fe5b7b
Luc <lucwsomers@gmail.com>
2019-07-08 10:38:25 -0300

    assign cookies leetcode

---
+++ leetcode/python/assign-cookies.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        satisfied = 0
+        while s and g:
+            if s[-1] < g[-1]:
+                g.pop()
+            else:
+                satisfied += 1
+                s.pop()
+                g.pop()
+        return satisfied
