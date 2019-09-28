8225eb5f10d433709432c95df2eff0794809eaef
Luc Somers <lucwsomers@gmail.com>
2019-09-28 10:58:15 -0300

    fix class name

--- leetcode/java/jewels-and-stones.java
+++ leetcode/java/jewels-and-stones.java
@@ -1,25 +1,25 @@
 import java.util.HashMap;
 
-class Solution {
-    public int numJewelsInStones(String J, String S) {
-        int count = 0;
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+class JewelsAndStones {
+  public int numJewelsInStones(String J, String S) {
+    int count = 0;
+    HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 
-        for (int i = 0; i < S.length(); i++) {
-            char c = S.charAt(i);
-            int total = map.containsKey(c) ? map.get(c) + 1 : 1;
-            map.put(c, total);
-        }
-
-        for (int i = 0; i < J.length(); i++) {
-            char c = J.charAt(i);
-            boolean inMap = map.containsKey(c);
-            if (inMap) {
-                int keyCount = map.get(c);
-                count += keyCount;
-            }
-        }
-
-        return count;
+    for (int i = 0; i < S.length(); i++) {
+      char c = S.charAt(i);
+      int total = map.containsKey(c) ? map.get(c) + 1 : 1;
+      map.put(c, total);
     }
+
+    for (int i = 0; i < J.length(); i++) {
+      char c = J.charAt(i);
+      boolean inMap = map.containsKey(c);
+      if (inMap) {
+        int keyCount = map.get(c);
+        count += keyCount;
+      }
+    }
+
+    return count;
+  }
 }
