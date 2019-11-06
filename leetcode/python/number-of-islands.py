d39eb4050d0e0225f504188e41b1bceb91af16f5
Luc Somers <lucwsomers@gmail.com>
2019-11-06 08:47:05 -0300

    num islands

---
+++ leetcode/python/number-of-islands.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numIslands(self, grid):
+        islands_count = 0
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] == '1':
+                    islands_count += 1
+                    self.callBFS(grid, i, j)
+
+        return islands_count
+    
+    def callBFS(self, grid, i, j):
+        # check boundaries
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[i]) or grid[i][j] == '0':
+            return
+        
+        grid[i][j] = '0'
+        self.callBFS(grid, i + 1, j) # up
+        self.callBFS(grid, i - 1, j) # down
+        self.callBFS(grid, i, j + 1) # left
+        self.callBFS(grid, i, j - 1) # right
+        
+        
