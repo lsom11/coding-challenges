093d4ebe9837d3cefd5e5550b58ceabf32fb8fb5
Luc Somers <lucwsomers@gmail.com>
2019-11-12 09:57:18 -0300

    plus one

---
+++ leetcode/python/plus-one.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def plusOne(self, digits):
+        n = len(digits)
+
+        for i in reversed(range(n)):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+            
+        new_digits = [1] + digits
+        
+        return new_digits
