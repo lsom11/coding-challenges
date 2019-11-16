87af0b630a7bc74a9e1bd330e3a44e4e7e4d2331
Luc Somers <lucwsomers@gmail.com>
2019-11-16 12:47:10 -0300

    occupations

--- leetcode/python/longest-common-prefix.py
+++ leetcode/python/longest-common-prefix.py
@@ -2,14 +2,15 @@ class Solution(object):
     def longestCommonPrefix(self, strs):
         if len(strs) == 0 or '' in strs:
             return ''
-
-        # get max possible prefix length
+        
         lens = [len(str) for str in strs]
         min_len, res = min(lens), ''
 
         for i in range(min_len):
             prefix = strs[0][:i + 1]
-            for s in strs:
+            for s in strs:             
+                # if the string isnt equal to the prefix 
+                # we break and return otherwise we set it to the prefix and go to next str
                 if s[:i + 1] != prefix:
                     return res
             res = prefix
