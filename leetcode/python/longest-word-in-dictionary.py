22f9e43ce880a0f2f89557491f275fd526746ad6
Luc <lucwsomers@gmail.com>
2019-11-18 20:37:05 -0300

    longest word

---
+++ leetcode/python/longest-word-in-dictionary.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        # sort so decomposed version comes first
+        words.sort()
+        
+        builtWords = set()
+        result  = ""
+        
+        for word in words:
+            substr = word[:-1]
+            # if length is 1 we can add to set or if the substr is in builtwords we can add
+            if len(word) == 1 or substr in builtWords:
+                # this check helps with words with same length, sorted alphabetically already
+                if len(word) > len(result): result = word
+                builtWords.add(word)
+            
+        return result
