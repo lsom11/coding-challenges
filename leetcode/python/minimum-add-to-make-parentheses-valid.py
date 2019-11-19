6d6282b3bda7fcd134704ee8349dd1e6eb4ba61e
Luc Somers <lucwsomers@gmail.com>
2019-11-19 08:36:34 -0300

    minimum add

--- leetcode/python/minimum-add-to-make-parentheses-valid.py
+++ leetcode/python/minimum-add-to-make-parentheses-valid.py
@@ -1,15 +1,19 @@
-class Solution:
-    def minAddToMakeValid(self, S: str) -> int:
-        count = 0
+class Solution(object):
+    def minAddToMakeValid(self, S):
         stack = []
-        for s in S:
-            # if left paren add to stack
-            if s == '(':
-                stack.append(s)
-            # if not left paren but stack has values pop from stack
-            elif len(stack) > 0:
+        # traverse and add to stack
+        for parens in S:
+            # if parens closed and open parens at top of stack, remove it, balanced
+            if parens == ')' and stack and stack[-1] == '(':
                 stack.pop()
-            # otherwise increment by 1    
+            # otherwise push to stack
             else:
-                count += 1
-        return count + len(stack)
+                stack.append(parens)
+        return len(stack)
+
+class Solution:
+    def minAddToMakeValid(self, S):
+        # if balanced bracket remove it, return the length of the unbalanced bracket
+        while '()' in S:
+            S = S.replace('()', '')
+        return len(S)
