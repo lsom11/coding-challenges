65ae6291d1b662f463504a023a304a3947afd54b
Luc Somers <lucwsomers@gmail.com>
2019-12-09 12:09:44 -0300

    retrieve data from table

---
+++ w3resource/sql/retrieve-data-from-table.sql
@@ -0,0 +1,22 @@
+-- 1. Write a SQL statement to display all the information of all salesmen. 
+
+SELECT * FROM salesman;
+
+-- 2. Write a SQL statement to display a string "This is SQL Exercise, Practice and Solution".
+
+SELECT 'This is SQL Exercise, Practice and Solution';
+
+-- 3. Write a query to display three numbers in three columns. 
+
+SELECT 5, 6, 7;
+
+-- 4. Write a query to display the sum of two numbers 10 and 15 from RDMS sever.  
+
+SELECT 10 + 15;
+
+-- 5. Write a query to display the result of an arithmetic expression. 
+
+SET @a = 5;
+SET @b = 6;
+
+SELECT a * b;
