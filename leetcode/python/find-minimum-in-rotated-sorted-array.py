cba84f16a5c2df6778c5137fc453b4ab61a04589
Luc Somers <lucwsomers@gmail.com>
2019-12-27 10:21:19 -0300

    minimum in sorted array

--- leetcode/python/find-minimum-in-rotated-sorted-array.py
+++ leetcode/python/find-minimum-in-rotated-sorted-array.py
@@ -8,6 +8,7 @@ class Solution:
         right = len(nums) - 1
         
         while left < right:
+            print(left, right)
             midpoint = int(left + (right - left) / 2)
             # find condition - if mid less that nums youve found the rotation
             if midpoint > 0 and nums[midpoint] < nums[midpoint - 1]: 
