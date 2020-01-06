459c9ac44b81b0f1dc063f043f5f134a5005fa1d
Luc Somers <lucwsomers@gmail.com>
2020-01-06 07:43:54 -0300

    array partition

---
+++ leetcode/python/array-partition-i.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def arrayPairSum(self, nums):
+        N = 2
+        sorted_nums = sorted(nums)
+        pairs = [sorted_nums[n:n+N] for n in range(0, len(sorted_nums), N)]
+        sum = 0
+        for pair in pairs:
+            first, second = pair
+            sum += min(first, second)
+        return sum
+
+test = Solution()
+test.arrayPairSum([[1,4,3,2]]) # 4
