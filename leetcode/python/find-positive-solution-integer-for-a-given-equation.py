690cbdc2e5acf4406fc7aaa92fa55b70810badd6
Luc Somers <lucwsomers@gmail.com>
2020-01-08 08:57:15 -0300

    positive integer solution

---
+++ leetcode/python/find-positive-solution-integer-for-a-given-equation.py
@@ -0,0 +1,21 @@
+"""
+   This is the custom function interface.
+   You should not implement it, or speculate about its implementation
+   class CustomFunction:
+       # Returns f(x, y) for any given positive integers x and y.
+       # Note that f(x, y) is increasing with respect to both x and y.
+       # i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+       def f(self, x, y):
+  
+"""
+class Solution(object):
+    def findSolution(self, customfunction, z):
+        A = []
+        for x in range(1, z + 1):
+            if customfunction.f(x, 1) > z:
+                return A
+            for y in range(1, z + 1):
+                if customfunction.f(x, y) == z:
+                    A.append([x, y])
+                    break
+        return A
