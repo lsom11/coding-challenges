6d5eafa4c251ffc45cf94d6a8bee78daf0e7a66f
Luc Somers <lucwsomers@gmail.com>
2020-01-10 08:35:35 -0300

    queens 2

--- leetcode/python/queens-that-can-attack-the-king.py
+++ leetcode/python/queens-that-can-attack-the-king.py
@@ -10,6 +10,7 @@ class Solution:
             while self.is_valid(curr_coordinates):
                 queen = str(curr_coordinates[0]) + str(curr_coordinates[1])
                 if queen in attacking_queens:
+                    print(queen)
                     attacking_queens[queen] = True
                     break
                 curr_coordinates = [curr_coordinates[0] + directions[i][0], curr_coordinates[1] + directions[i][1]]
