c68a22c1b0477044034e14533961071bd787b5a3
Luc Somers <lucwsomers@gmail.com>
2020-01-16 08:46:04 -0300

    code wars challenges

---
+++ codewars/python/who-likes-it.py
@@ -0,0 +1,6 @@
+def likes(names):
+    if not len(names): return "no one likes this"
+    elif len(names) == 1: return "{} likes this".format(names[0])
+    elif len(names) == 2: return "{} and {} like this".format(names[0], names[1])
+    elif len(names) == 3: return "{}, {} and {} like this".format(names[0], names[1], names[2])
+    else: return "{}, {} and {} others like this".format(names[0], names[1], (len(names) - 2))
---
+++ codewars/sql/SQL-basics-simple-group-by.sql
@@ -0,0 +1,2 @@
+SELECT age, COUNT(*) as people_count FROM people
+GROUP BY age
