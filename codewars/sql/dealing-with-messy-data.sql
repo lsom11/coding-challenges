48d2fce83234f33f41bd937aae1a8300aa59a53c
Luc Somers <lucwsomers@gmail.com>
2020-02-03 08:58:17 -0300

    messy data

---
+++ codewars/sql/dealing-with-messy-data.sql
@@ -0,0 +1,11 @@
+SELECT
+  c.first_name,
+  c.last_name,
+  c.credit_limit AS "old_limit",
+  max(p.credit_limit) AS "new_limit"
+FROM customers c, prospects p
+WHERE lower(p.full_name) LIKE '%' || lower(c.first_name) || '%'
+AND lower(p.full_name) LIKE '%' || lower(c.last_name) || '%'
+AND p.credit_limit > c.credit_limit
+GROUP BY c.first_name, c.last_name, c.credit_limit
+ORDER BY first_name;
