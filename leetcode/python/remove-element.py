48d5a43cf45efa4d50c81b1b30135a3099c62c6e
Luc Somers <lucwsomers@gmail.com>
2020-03-30 10:36:58 -0300

    remove element

--- leetcode/python/remove-element.py
+++ leetcode/python/remove-element.py
@@ -6,6 +6,7 @@ class Solution:
         
         for i in range(len(nums)):
             if nums[i] != val:
+                print(nums[valid_size])
                 nums[valid_size] = nums[i]
                 valid_size += 1
         
