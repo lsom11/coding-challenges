0faf5f635a4baae323ed2dfc51f56f1644e38f4a
Luc Somers <lucwsomers@gmail.com>
2020-04-20 11:18:00 -0300

    bst from preorder

---
+++ leetcode/30-day-leetcoding-challenge/Construct Binary Search Tree from Preorder Traversal.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        i = 1
+        while i<len(preorder) and preorder[i] < root.val:
+            i+=1
+        root.left = self.bstFromPreorder(preorder[1:i])
+        root.right = self.bstFromPreorder(preorder[i:])
+        return root
