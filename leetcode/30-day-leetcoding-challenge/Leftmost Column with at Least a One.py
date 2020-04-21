23d2b8c11f2526a35e414ee7d83e4fd409c7935c
Luc Somers <lucwsomers@gmail.com>
2020-04-21 09:34:21 -0300

    leftmost column

---
+++ leetcode/30-day-leetcoding-challenge/Leftmost Column with at Least a One.py
@@ -0,0 +1,23 @@
+# """
+# This is BinaryMatrix's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class BinaryMatrix(object):
+#    def get(self, x: int, y: int) -> int:
+#    def dimensions(self) -> list[]:
+
+class Solution:
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        rows, cols = binaryMatrix.dimensions()
+
+        current_row = 0
+        current_col = cols - 1
+        while current_row < rows and current_col >= 0:
+            if binaryMatrix.get(current_row, current_col) == 0:
+                current_row += 1
+            else:
+                current_col -= 1
+        if current_col != cols - 1:
+            return current_col + 1
+        else:
+            return -1
