8742c012f4e7b8e9d5583e935189ce5f48b4755c
Luc Somers <lucwsomers@gmail.com>
2020-04-27 11:34:20 -0300

    maximal square

---
+++ leetcode/30-day-leetcoding-challenge/Maximal Square.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        n, m = len(matrix), len(matrix[0])
+        mat2 = [[0 for _ in range(m + 1)] for __ in range(n + 1)]
+        for i in range(n):
+            for j in range(m):
+                mat2[i][j] = min(mat2[i - 1][j - 1], mat2[i - 1][j], mat2[i][j - 1]) + 1 if matrix[i][j] == '1' else 0
+        return max([max(row) for row in mat2]) ** 2
