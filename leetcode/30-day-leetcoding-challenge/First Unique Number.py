1215dd6ed3c66626bdc011331ec9b482ea8bceec
Luc Somers <lucwsomers@gmail.com>
2020-04-28 10:28:10 -0300

    first unique number

---
+++ leetcode/30-day-leetcoding-challenge/First Unique Number.py
@@ -0,0 +1,29 @@
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.q = []
+        self.dict = {}
+        for i in nums:
+            self.add(i)
+
+    def showFirstUnique(self) -> int:
+        while len(self.q) > 0 and self.dict[self.q[0]] > 1:
+            self.q.pop(0)
+        if len(self.q) == 0:
+            return -1
+        else:
+            return self.q[0]
+
+    def add(self, value: int) -> None:
+        if value in self.dict:
+            self.dict[value] += 1
+        else:
+            self.dict[value] = 1
+            self.q.append(value)
+        
+
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
