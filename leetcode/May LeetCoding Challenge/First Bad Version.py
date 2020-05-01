8fd09ecacaa680e7136b9b2360d2a9ceb578f568
Luc Somers <lucwsomers@gmail.com>
2020-05-01 12:35:45 -0300

    first bad version

---
+++ leetcode/May LeetCoding Challenge/First Bad Version.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left , right = 1 , n
+        while left <= right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                ans = mid
+                right = mid - 1
+            else :left = mid + 1
+        return ans
