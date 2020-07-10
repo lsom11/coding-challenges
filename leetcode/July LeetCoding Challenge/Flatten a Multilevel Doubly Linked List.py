afe5578a87660727c4c1ddac500afa69614267a9
Luc Somers <lucwsomers@gmail.com>
2020-07-10 08:51:01 -0300

    double linked

---
+++ leetcode/July LeetCoding Challenge/Flatten a Multilevel Doubly Linked List.py
@@ -0,0 +1,19 @@
+class Solution:
+    def flatten(self, head):
+        if not head: return head
+        stack, order = [head], []
+
+        while stack:
+            last = stack.pop()
+            order.append(last)
+            if last.next:
+                stack.append(last.next)
+            if last.child:
+                stack.append(last.child)
+        
+        for i in range(len(order) - 1):
+            order[i+1].prev = order[i]
+            order[i].next = order[i+1]
+            order[i].child = None
+            
+        return order[0]
