9be5d84ec2b4e1c0dbdca3dad2da789ac2881043
Luc Somers <lucwsomers@gmail.com>
2020-07-16 10:48:19 -0300

    pow x

---
+++ leetcode/July LeetCoding Challenge/Pow(x, n).py
@@ -0,0 +1,8 @@
+class Solution:
+    def myPow(self, x, n):
+        if abs(x) < 1e-40: return 0 
+        if n == 0: return 1
+        if n < 0: return self.myPow(1/x, -n)
+        lower = self.myPow(x, n//2)
+        if n % 2 == 0: return lower*lower
+        if n % 2 == 1: return lower*lower*x
