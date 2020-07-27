bf47708cccc23999558e6bb0212be62124e14bf4
Luc Somers <lucwsomers@gmail.com>
2020-07-27 12:58:53 -0300

    bin tree from inorder

---
+++ leetcode/July LeetCoding Challenge/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -0,0 +1,13 @@
+class Solution:
+    def buildTree(self, inorder, postorder):
+        def helper(post_beg, post_end, in_beg, in_end):
+            if post_end - post_beg <= 0: return None
+            ind = dic[postorder[post_end-1]]
+
+            root = TreeNode(inorder[ind])  
+            root.left  = helper(post_beg, post_beg + ind - in_beg, in_beg, ind)
+            root.right = helper(post_end - in_end + ind, post_end - 1, ind + 1, in_end)
+            return root
+        
+        dic = {elem: it for it, elem in enumerate(inorder)}  
+        return helper(0, len(postorder), 0, len(inorder))
