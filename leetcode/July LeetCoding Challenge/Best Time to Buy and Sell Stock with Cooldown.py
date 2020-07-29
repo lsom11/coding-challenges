0f97431c9767e04d59e30604a7b223e968c6a8fe
Luc Somers <lucwsomers@gmail.com>
2020-07-29 13:39:49 -0300

    stock sell

---
+++ leetcode/July LeetCoding Challenge/Best Time to Buy and Sell Stock with Cooldown.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxProfit(self, prices):
+        n = len(prices)
+        if n <= 1: return 0
+        
+        diff = [prices[i+1] - prices[i] for i in range(n-1)]
+        dp, dp_max = [0]*(n + 1), [0]*(n + 1)
+        for i in range(n-1):
+            dp[i] = diff[i] + max(dp_max[i-3], dp[i-1])
+            dp_max[i] = max(dp_max[i-1], dp[i])
+            
+        return dp_max[-3]
