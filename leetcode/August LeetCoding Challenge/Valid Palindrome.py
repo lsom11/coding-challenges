88d2b541580cfd1c7841b9e97833caf213f34655
Luc Somers <lucwsomers@gmail.com>
2020-08-03 10:30:21 -0300

    valid palindrome

---
+++ leetcode/August LeetCoding Challenge/Valid Palindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s):
+        beg, end = 0, len(s) - 1
+        while beg <= end:
+            while not s[beg].isalnum() and beg < end: beg += 1
+            while not s[end].isalnum() and beg < end: end -= 1
+            if s[beg] == s[end] or s[beg].upper() == s[end].upper():
+                beg, end = beg + 1, end - 1
+            else:
+                return False
+        return True
