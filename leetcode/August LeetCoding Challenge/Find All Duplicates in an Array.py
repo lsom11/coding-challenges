015295e60acbaa95d9af25c36e44d9a01f2ac0e2
Luc Somers <lucwsomers@gmail.com>
2020-08-06 15:10:16 -0300

    all duplicates

---
+++ leetcode/August LeetCoding Challenge/Find All Duplicates in an Array.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        for i in range(len(nums)):
+            while i != nums[i] - 1 and nums[i] != nums[nums[i]-1]:
+				nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+
+        return [nums[it] for it in range(len(nums)) if it != nums[it] - 1]
