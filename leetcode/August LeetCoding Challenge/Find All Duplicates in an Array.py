b6157804c45f0012c0c57e39f1e51efafb277209
Luc Somers <lucwsomers@gmail.com>
2020-08-16 11:06:54 -0300

    fix algo

--- leetcode/August LeetCoding Challenge/Find All Duplicates in an Array.py
+++ leetcode/August LeetCoding Challenge/Find All Duplicates in an Array.py
@@ -3,5 +3,4 @@ class Solution(object):
         for i in range(len(nums)):
             while i != nums[i] - 1 and nums[i] != nums[nums[i]-1]:
 				nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
-        print[nums[it]]
         return [nums[it] for it in range(len(nums)) if it != nums[it] - 1]
