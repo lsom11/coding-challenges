842f2e39b1b1271d423c22fc2f3b1562fda69eba
Luc Somers <lucwsomers@gmail.com>
2020-08-31 15:06:22 -0300

    good pairs

---
+++ leetcode/python/Number of Good Pairs.py
@@ -0,0 +1,8 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        if len(nums) == len(set(nums)):
+            return 0
+        sum = 0
+        for i,n in enumerate(nums):
+            sum += nums[i:].count(n) -1
+        return sum
