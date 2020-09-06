af4811318f2160b196a6dbe4195785b70fd1db62
Luc Somers <lucwsomers@gmail.com>
2020-09-06 14:45:26 -0300

    image overlap

---
+++ leetcode/September LeetCoding Challenge/Image Overlap.py
@@ -0,0 +1,7 @@
+from scipy.ndimage import convolve
+import numpy as np
+
+class Solution:
+    def largestOverlap(self, A, B):
+        B = np.pad(B, len(A), mode='constant', constant_values=(0, 0))
+        return np.amax(convolve(B, np.flip(np.flip(A,1),0), mode='constant'))
