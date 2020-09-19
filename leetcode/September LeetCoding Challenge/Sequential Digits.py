71b0f164b4eae8bb734e1b51dad26d1c0124de2a
Luc Somers <lucwsomers@gmail.com>
2020-09-19 11:07:50 -0300

    seq digits

---
+++ leetcode/September LeetCoding Challenge/Sequential Digits.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sequentialDigits(self, low, high):
+        out = []
+        queue = deque(range(1,10))
+        while queue:
+            elem = queue.popleft()
+            if low <= elem <= high:
+                out.append(elem)
+            last = elem % 10
+            if last < 9: queue.append(elem*10 + last + 1)
+                    
+        return out
