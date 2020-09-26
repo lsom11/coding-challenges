fc898dd8b2f93305695c4b7847e6f9bf69e17739
Luc Somers <lucwsomers@gmail.com>
2020-09-26 14:45:06 -0300

    large number 2

--- leetcode/September LeetCoding Challenge/Largest Number.py
+++ leetcode/September LeetCoding Challenge/Largest Number.py
@@ -1,4 +1,5 @@
 class Solution:
     def largestNumber(self, nums):
         compare = lambda a, b: -1 if a+b > b+a else 1 if a+b < b+a else 0
+        print(compare)
         return str(int("".join(sorted(map(str, nums), key = cmp_to_key(compare)))))
