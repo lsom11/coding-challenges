beaebeb209a2d5f920ebcf0c21dcb932246599a8
Luc Somers <lucwsomers@gmail.com>
2020-10-03 11:45:52 -0300

    k diff

---
+++ leetcode/October LeetCoding Challenge/K-diff Pairs in an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findPairs(self, nums, k):
+        count = Counter(nums)
+        if k > 0:
+            res = sum([i + k in count for i in count])
+        else:
+            res = sum([count[i] > 1 for i in count])
+        return res
