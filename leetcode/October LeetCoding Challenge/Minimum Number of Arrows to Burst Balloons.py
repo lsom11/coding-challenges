37780532f8e15cd9155854074633e975c71afb9f
Luc Somers <lucwsomers@gmail.com>
2020-10-10 14:03:44 -0300

    number of arrows

---
+++ leetcode/October LeetCoding Challenge/Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMinArrowShots(self, points):
+        points.sort(key = lambda x: x[1])
+        n, count = len(points), 1
+        if n == 0: return 0
+        curr = points[0]
+        
+        for i in range(n):
+            if curr[1] < points[i][0]:
+                count += 1
+                curr = points[i]
+                
+        return count  
