47e00883d7e2adc7c57a91667945d8b221bce1f0
Luc Somers <lucwsomers@gmail.com>
2020-12-02 09:10:48 -0300

    most cnadies

---
+++ leetcode/python/Kids With the Greatest Number of Candies.py
@@ -0,0 +1,10 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        max_candy = max(candies)
+        res = []
+        for i in range(len(candies)):
+            if (candies[i] + extraCandies) >= max_candy:
+                res.append(True)
+            else:
+                res.append(False)
+        return res```
