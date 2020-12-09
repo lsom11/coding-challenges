eae8303d1f7a2ec49ad91c9bcd72dbb327c2789d
Luc Somers <lucwsomers@gmail.com>
2020-12-09 14:18:29 -0300

    fix

--- leetcode/python/Pairs of Songs With Total Durations Divisible by 60.py
+++ leetcode/python/Pairs of Songs With Total Durations Divisible by 60.py
@@ -4,4 +4,5 @@ class Solution:
         for t in time: d[t%T] += 1
         ans = sum(d.get(i,0)*d.get(T-i,0) for i in range(1, T//2))
         q1, q2 = d.get(0,0), d.get(T//2, 0)
+        print(ans)
         return ans + q1*(q1-1)//2 + q2*(q2-1)//2
