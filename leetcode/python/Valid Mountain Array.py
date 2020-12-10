20dbcd309c5ef32c21a3794f0f737ab771446972
Luc Somers <lucwsomers@gmail.com>
2020-12-10 11:39:19 -0300

    valid mountain

---
+++ leetcode/python/Valid Mountain Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def validMountainArray(self, arr):
+        n = len(arr)
+        beg, end = 0, n - 1
+        while beg != n-1 and arr[beg + 1] > arr[beg]: beg += 1
+        while end != 0 and arr[end - 1] > arr[end]: end -= 1 
+        return beg == end and end != n-1 and beg != 0
