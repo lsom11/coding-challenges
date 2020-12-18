e3d04b7deafdc781bb4e09cf1a2000bb38bc3a9f
Luc Somers <lucwsomers@gmail.com>
2020-12-18 10:29:13 -0300

    4 sum

---
+++ leetcode/python/4Sum II.py
@@ -0,0 +1,14 @@
+class Solution:
+    def fourSumCount(self, A, B, C, D):
+        Cnt1, Cnt2, ans = Counter(), Counter(), 0
+        for a, b in product(A, B):
+            Cnt1[a + b] += 1
+            
+        for c, d in product(C, D):
+            Cnt2[c + d] += 1
+            
+        for val in Cnt1:
+            if -val in Cnt2:
+                ans += Cnt1[val]*Cnt2[-val]
+                
+        return ans
