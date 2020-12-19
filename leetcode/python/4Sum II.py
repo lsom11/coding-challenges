46d1092d261d9f78c39084c7bcd33e6a715664bd
Luc Somers <lucwsomers@gmail.com>
2020-12-19 17:41:51 -0300

    4sum2

--- leetcode/python/4Sum II.py
+++ leetcode/python/4Sum II.py
@@ -3,7 +3,7 @@ class Solution:
         Cnt1, Cnt2, ans = Counter(), Counter(), 0
         for a, b in product(A, B):
             Cnt1[a + b] += 1
-            
+            print(Cnt1)
         for c, d in product(C, D):
             Cnt2[c + d] += 1
             
