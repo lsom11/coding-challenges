4c887e04215c9fa9cd1ef3309521f9611f3ce9d6
Luc Somers <lucwsomers@gmail.com>
2020-12-21 12:10:52 -0300

    day of week

--- leetcode/python/day-of-the-week.py
+++ leetcode/python/day-of-the-week.py
@@ -4,5 +4,6 @@ import calendar
 class Solution(object):
     def dayOfTheWeek(self, day, month, year):
         my_date = datetime.datetime(year, month, day)
+        print(my_date)
         return calendar.day_name[my_date.weekday()] 
         
